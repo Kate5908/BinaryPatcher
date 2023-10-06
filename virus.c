bc5531e2197277d85192da2cfb8300bfc653c7d3
Kate <z5417428@ad.unsw.edu.au>
2023-10-06 19:47:02 +1100

    Created boolean types for virus

---
+++ virus.c
@@ -0,0 +1,21 @@
+// virus.c
+// written by Kate Rodionoff
+// on 06/10/23
+// virus that infects ELF executable files on Linux
+
+// verify that file is an ELF executable
+// find entry point
+// inject simple code (to start with)
+
+// no libraries
+
+typedef unsigned int bool;
+
+// redefine booleans because no libraries here ;-;
+#define true 1
+#define false 0
+
+int main(void) {
+
+}
+
