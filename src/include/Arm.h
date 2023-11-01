79f3bd9b8164f5fc3c887df0e9b7a4ab20168c9b
Kate <z5417428@ad.unsw.edu.au>
2023-11-02 01:26:14 +1100

    Added br instruction to Arm.h

--- src/include/Arm.h
+++ src/include/Arm.h
@@ -8,6 +8,8 @@
 #define MOV "\x16\x00\x80\xd2"
 // ADRP x22, #0x0 => use this for PIE executables
 #define ADRP "\x16\x00\x00\x90"
+// BR x22
+#define BR "\xc0\x02\x1f\xd6"
 
 char *add(uint32_t imm);
 char *immediateUnshifted(uint32_t imm);
