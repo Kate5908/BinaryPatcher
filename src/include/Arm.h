71896504a8fd1a4d6ed03aacfb867f95c48969f0
Kate <z5417428@ad.unsw.edu.au>
2023-11-02 14:32:04 +1100

    Added documentation to Arm.h

--- src/include/Arm.h
+++ src/include/Arm.h
@@ -13,9 +13,34 @@
 
 #define LDR "\x16\x00\x00\x58"
 
+// encodes the instruction ADD x22, x22, [imm] in ARM64
 char *add(uint32_t imm);
+
+/**
+ * The following two function signatures perform the same operation
+ * but encode different values
+ * 
+ * For a value between 0x0 and 0xfff (inclusive) we can directly encode the immediate
+ * so we set the shift bit to be 0
+ * The instruction is written in assembly as ADD [Rd], [Rn], [Imm]
+ * 
+ * For a value greater than 0xfff, we need to apply a bit shift in order to fit
+ * the immediate into a 2 byte encoding
+ * 
+ * We set the shift bit to 1 (hence the need for separate functions)
+ * The assembly instruction would be ADD [Rd], [Rn], [Imm / 0x1000], lsl #12
+ * 
+ * It is impossible to use values like 0x400580 in an add instruction directly
+ * We would instead need to break it up into two separate ADD instructions
+ *      ADD x0, x0, #0x400000, lsl #12
+ *      ADD x0, x0, #0x580
+ */
+
+// calculates the immediate for an unshifted ADD instruction
 char *immediateUnshifted(uint32_t imm);
+// calculates the immediate for a shifted ADD instruction
 char *immediateShifted(uint32_t imm);
+// encodes the instruction SUB x22, x22, [imm] in ARM64
 char *sub(uint32_t imm);
 
 #endif
